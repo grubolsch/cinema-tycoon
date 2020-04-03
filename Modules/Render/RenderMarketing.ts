dc4e63b6cb91c2665f5e6a211bd3d1839ae26cde
Janvdv96 <janvandevelde.dev@gmail.com>
2020-04-03 11:22:21 +0200

    updated slider code

--- Modules/Render/RenderMarketing.ts
+++ Modules/Render/RenderMarketing.ts
@@ -28,29 +28,59 @@ class RenderMarketing implements RenderInterface {
 
     public renderOneTime() {
         document.getElementById('marketingDurationRange')!.addEventListener('input', (e) => {
-            // @ts-ignore
-            document.getElementById('MarketingDurationPreview')!.innerText = e.target.value;
+            this.updateMarketingSlider(e);
         });
         document.getElementById('marketingSubmit')!.addEventListener('click', () => {
-            // @ts-ignore
-            let rangeValue = document.getElementById('marketingDurationRange').value;
-            rangeValue = this._cinema.marketingManager.checkWeekRangeMinMaxValue(rangeValue);
-
-            let optMarketing = document.getElementsByName('optMarketing');
-            let selectedOption : string = "";
-            optMarketing.forEach(option => {
+            this.submitMarketingChoice();
+        });
+        let optMarketing = document.getElementsByName('optMarketing');
+        optMarketing.forEach(option => {
+            option.addEventListener('input', () => {
                 // @ts-ignore
-                if (option.checked){
-                    // @ts-ignore
-                    selectedOption = option.value;
-                }
-            });
-            // @ts-ignore
-            if (TYPES[selectedOption] !== null){
-                let campaign = this._cinema.marketingManager.createCampaign(selectedOption, rangeValue);
-                this._cinema.marketingManager.startCampaign(campaign, this._cinema);
-            }
+                document.getElementById('MarketingDurationPreview')!.innerText = 1;
+                // @ts-ignore
+                document.getElementById('marketingDurationRange').value = 1
+            })
         })
+
+    }
+
+    private submitMarketingChoice() {
+        // @ts-ignore
+        let rangeValue = document.getElementById('marketingDurationRange').value;
+        rangeValue = this._cinema.marketingManager.checkWeekRangeMinMaxValue(rangeValue);
+
+        let optMarketing = document.getElementsByName('optMarketing');
+        let selectedOption: string = "";
+        optMarketing.forEach(option => {
+            // @ts-ignore
+            if (option.checked) {
+                // @ts-ignore
+                selectedOption = option.value;
+            }
+        });
+        // @ts-ignore
+        if (TYPES[selectedOption] !== null) {
+            let campaign = this._cinema.marketingManager.createCampaign(selectedOption, rangeValue);
+            this._cinema.marketingManager.startCampaign(campaign, this._cinema);
+        }
+    }
+
+    private updateMarketingSlider(e: Event) {
+        let optMarketing = document.getElementsByName('optMarketing');
+        let calculatedCost: number;
+        optMarketing.forEach(option => {
+            let costPerWeek: number;
+            // @ts-ignore
+            if (option.checked && option.dataset.cost) {
+                // @ts-ignore
+                costPerWeek = option.dataset.cost;
+                // @ts-ignore
+                calculatedCost = +e.target.value * costPerWeek;
+            }
+            // @ts-ignore
+            document.getElementById('MarketingDurationPreview')!.innerText = e.target.value + " -> cost: " + calculatedCost;
+        });
     }
 }
 
