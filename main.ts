c7cf54fa45398caa4e731d87b086c87160ef0a0c
Koen Eelen <koen@becode.org>
2020-04-08 20:40:13 +0200

    Created genre manager

--- main.ts
+++ main.ts
@@ -2,7 +2,6 @@ import {Observer} from "./Modules/Manager/Observer";
 import {ConfigManager} from "./Modules/Manager/ConfigManager";
 import {TimeManager} from "./Modules/Manager/TimeManager";
 import {Cinema} from "./Modules/Entity/Cinema";
-import {CustomerGenerator} from "./Modules/Generator/CustomerGenerator";
 import {Render} from "./Modules/Render/Render";
 import {FinanceManager} from "./Modules/Manager/FinanceManager";
 import {MarketingManager} from "./Modules/Manager/MarketingManager";
@@ -10,20 +9,16 @@ import {LoanManager} from "./Modules/Manager/LoanManager";
 import {RenderLoans} from "./Modules/Render/RenderLoans";
 import {RenderBoots} from "./Modules/Render/RenderBoots";
 import {RenderMarketing} from "./Modules/Render/RenderMarketing";
-import {Customer} from "./Modules/Entity/Customer";
 import {MovieGenerator} from "./Modules/Generator/MovieGenerator";
 import {Movie} from "./Modules/Entity/Movie";
 import {RenderResearch} from "./Modules/Render/RenderResearch";
 import {ResearchItem} from "./Modules/Entity/Research/ResearchItem";
+import {GenreManager} from "./Modules/Manager/GenreManager";
 
-function init() {
-    generateMovie();
-}
-
-function generateMovie() {
+function generateMovies(genreManager : GenreManager) : void {
     let manyMovies: Array<Movie> = [];
     for (let i = 0; i < 10; i++){
-        manyMovies[i] = MovieGenerator.newMovie();
+        manyMovies[i] = MovieGenerator.newMovie(genreManager);
     }
     console.log(manyMovies);
 }
@@ -31,10 +26,11 @@ function generateMovie() {
 const observer = new Observer;
 const configManager = new ConfigManager;
 const loanManager = new LoanManager;
+const genreManager = new GenreManager(configManager);
 
 document.addEventListener('DOMContentLoaded', () => {
     // temporary code, this should come from a save or a "create new game" menu
-    init();
+    generateMovies(genreManager);
 
     let cinema = new Cinema("Our own Cinema", new TimeManager(observer), configManager, new FinanceManager(configManager), new MarketingManager());
 
@@ -57,6 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //observers
     observer.subscribe(observer.MONTH, () => {
         loanManager.update(cinema);
+        genreManager.update();
         cinema.researchManager.update(observer);
 
         render.renderByMonth();
@@ -82,7 +79,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     observer.subscribe(observer.RESEARCH_FINISHED, function(params: { research: ResearchItem; }) {
         alert('You finished research on '+ params.research.name + '. Make sure you select a new technology to work on. \nStanding still is going backwards.');
-
+    });
 
     //end observers code
 
