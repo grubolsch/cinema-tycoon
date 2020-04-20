fe3a2d06910f004aa31a4b1d52614971464fa7ce
Janvdv96 <janvandevelde.dev@gmail.com>
2020-04-20 12:52:04 +0200

    removed movie from free ticket for now

--- Modules/Entity/FreeTicket.ts
+++ Modules/Entity/FreeTicket.ts
@@ -1,17 +1,7 @@
-import {Movie} from "./Movie";
 import {InventoryItem} from "./InventoryItem";
 
 class FreeTicket extends InventoryItem{
-    private readonly _movie : Movie;
 
-    constructor(movie: Movie) {
-        super();
-        this._movie = movie;
-    }
-
-    get movie(): Movie {
-        return this._movie;
-    }
 }
 
 export {FreeTicket}
